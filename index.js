b425fb9f7dcc36d1b47ba164be5f9481cb464327
Kajal <kajal.sekhri@codebrewinnovations.com>
2024-03-06 18:48:43 +0530

    edit with mongoose

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express"); 
 const app = express(); 
+app.use(express.json())
 // const swaggerUi=require('swagger-ui-express')
 // const swaggerFile=require('./swagger_output.json')
 // require('./endpoints')(app)
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // require('dotenv').config()
 
 // app.use('/doc',swaggerUi.serve,swaggerUi.setup(swaggerFile))
+const MyModel = mongoose.model('MyModel', {}, 'mycollection');
 
 app.get("/", (req, res) => { 
     res.send("Express on Vercel");
@@ -15,6 +17,19 @@ app.get("/", (req, res) => {
 app.get("/test", (req, res) => { 
     res.send("Express on test");
 }); 
+
+app.post('/user',(req,res)=>{
+    console.log(req.body,"req")
+    const username=req.body.name;
+    const age=req.body.age;
+    console.log(username,age)
+    MyModel.create({name:username,age:age}).then((ans) => { 
+        console.log("Document inserted") 
+    }).catch((err) => { 
+        console.log(err.Message); 
+    })
+    res.send("sent!")
+})
 
 // console.log(process.env.MONGODB_URI,">>>")
 // mongoose.connect(process.env.MONGODB_URI)
