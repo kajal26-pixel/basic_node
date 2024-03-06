9cf28ea2d9883a5ca5766ea7cb2feaedb5e5fdfa
Kajal <kajal.sekhri@codebrewinnovations.com>
2024-03-06 13:00:21 +0530

    changes made

--- endpoints.js
+++ endpoints.js
@@ -1,4 +1,14 @@
+const mongoose = require('mongoose');
+//const bodyParser=require('body-parser')
+const express=require('express')
+
+
+
 module.exports=function(app){
+
+    //app.use(bodyParser.urlencoded({extended:true}))
+    app.use(express.json())
+
     app.get('/users',(req,res)=>{
         res.send('hiiii')
     })
@@ -6,5 +16,20 @@ module.exports=function(app){
     app.get('/data',(req,res) =>{
         res.send({name:"kajal",designation:"Node js Developer",department:"Web"})
     })
+
+    app.post('/user',(req,res)=>{
+        console.log(req.body,"req")
+        const username=req.body.name;
+        const age=req.body.age;
+        console.log(username,age)
+        usermodel.create({name:username,age:age}).then((ans) => { 
+            console.log("Document inserted") 
+          }).catch((err) => { 
+            console.log(err.Message); 
+          })
+        res.send("sent!")
+    })
     
 }
+
+const usermodel=mongoose.model("users",new mongoose.Schema({name:{type:String},age:{type:Number}}))
--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const swaggerUi=require('swagger-ui-express')
 const swaggerFile=require('./swagger_output.json')
 require('./endpoints')(app)
+const mongoose = require('mongoose');
 
 app.use('/doc',swaggerUi.serve,swaggerUi.setup(swaggerFile))
 
 app.get("/", (req, res) => { 
     res.send("Express on Vercel");
 }); 
+
+
+mongoose.connect("mongodb://0.0.0.0:27017/vercel")
+.then(()=>{console.log("mongo connected!")})
+.catch((err)=>{console.error(err)})
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => { 
