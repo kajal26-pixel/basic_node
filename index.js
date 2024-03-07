e4f140fe569dda9a66289e44c674ae58bbe21b07
Kajal <kajal.sekhri@codebrewinnovations.com>
2024-03-07 11:24:45 +0530

    removed both imports

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ app.use(express.json())
 // const swaggerFile=require('./swagger_output.json')
 // require('./endpoints')(app)
 // const mongoose = require('mongoose');
-import mongodb from "mongodb"
-import mongoose from "mongoose";
+// import mongodb from "mongodb"
+// import mongoose from "mongoose";
 const bodyParser=require('body-parser')
 // require('dotenv').config()
 
